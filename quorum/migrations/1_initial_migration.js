6d1b0386c842e447a289b20870b78ecb6e01d7ec
yunxi.zhang <yunxi.zhang@accenture.com>
2020-07-07 20:09:33 +0100

    update truffle-config and migration file

--- quorum/migrations/1_initial_migration.js
+++ quorum/migrations/1_initial_migration.js
@@ -1,5 +1,11 @@
 const Migrations = artifacts.require("Migrations");
+const Web3 = require("web3");
+const web3 = new Web3(process.env.PROVIDER);
+const my_address = process.env.ADDRESS;
+const my_password = process.env.PASSWORD;
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+  web3.eth.personal.unlockAccount(my_address, my_password, 9000).then(async function (res) {
+    await deployer.deploy(Migrations);
+  });
 };
