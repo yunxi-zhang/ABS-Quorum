918ca85f5728dd72a564127c5baac9f39ac2a655
yunxi.zhang <yunxi.zhang@accenture.com>
2020-07-12 15:42:08 +0100

    add app service insight support for bank

--- webapps/bank/routes/bank.js
+++ webapps/bank/routes/bank.js
@@ -3,6 +3,20 @@ var cors = require('cors');
 var router = express.Router();
 router.use(express.json());
 router.use(cors());
+const dotenv = require('dotenv');
+dotenv.config();
+let appInsights = require('applicationinsights');
+appInsights.setup()
+.setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(true)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+    .start();
 
 router.get("/sellerBalance", async (req, res, next) => {
     res.json({"api":"sellerBalance"});
