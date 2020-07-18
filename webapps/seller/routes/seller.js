89c47ef21bbeb3b51ee8efb476da1465110ff0a7
yunxi.zhang <yunxi.zhang@accenture.com>
2020-07-18 20:21:10 +0100

    add apis for get balance and update balance for seller

--- webapps/seller/routes/seller.js
+++ webapps/seller/routes/seller.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var cors = require('cors');
 var router = express.Router();
+const sellerContract = require('../invokeContracts/sellerContract');
 router.use(express.json());
 router.use(cors());
 
 router.get("/balance", async (req, res, next) => {
-    res.json({"api":"balance"});
+    const balance = await sellerContract.getBalance();
+    res.json({"balance": balance});
+});
+
+router.post("/updateBalance", async (req, res) => {
+    const contractReceipt = await sellerContract.updateBalance(req.body.value);
+    res.json({"status": 'success', transactionId: contractReceipt})
 });
 
 module.exports = router;
