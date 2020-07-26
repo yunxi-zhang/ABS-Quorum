99df86f0bffee11ddc476fb12a4c52c7ae61c8ae
yunxi.zhang <yunxi.zhang@accenture.com>
2020-07-26 21:04:29 +0100

    update config file

--- quorum/config/config.js
+++ quorum/config/config.js
@@ -2,9 +2,9 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const privateTxPublicKeys = {
+    bankNodePubKey: process.env.BANKNODE_PUBKEY,
     buyerNodePubKey: process.env.BUYERNODE_PUBKEY,
-    supplierNodePubKey: process.env.SUPPLIERNODE_PUBKEY,
-    warehouseNodePubKey: process.env.WAREHOUSENODE_PUBKEY
+    supplierNodePubKey: process.env.SUPPLIERNODE_PUBKEY
 };
 const absConfig = {
     address: process.env.ADDRESS,
