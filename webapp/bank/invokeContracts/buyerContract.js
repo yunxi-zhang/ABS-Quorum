1e95d1b27f000e3d6dd10fbefe48bb4f6c17412b
yunxi.zhang <yunxi.zhang@accenture.com>
2020-09-07 13:22:44 +0100

    add logs for troubleshooting

--- webapp/bank/invokeContracts/buyerContract.js
+++ webapp/bank/invokeContracts/buyerContract.js
@@ -7,9 +7,11 @@ const web3 = getWeb3();
 const getBalance = async () => {
     try {
         return await web3.eth.personal.unlockAccount(ADDRESS, PASSWORD, 36000).then(async function (res) {
+            console.log("1");
             const myContract = await getContract("Buyer");
+            console.log("2");
             let balance = await myContract.methods.retreiveBalance().call();
-            return balance;
+            console.log("3")
         });
     } catch (e) {
         console.log(`Error in getBalance function in BuyerContract: ${e}`);
